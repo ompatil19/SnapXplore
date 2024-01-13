52e78b5c5be96698797af872070d80fd9a48d7be
ompatil19 <ompatil2002@gmail.com>
2024-01-13 21:08:06 +0530

    inserted get request for links

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import { ChakraProvider } from '@chakra-ui/react'
 import Home from './components/home/Home';
+import Reccom from './components/reccomendation/Reccomendation'
 import Uploader from './components/uploader/Uploader';
 function App() {
   return (
     <>
+    {/* <Reccom/> */}
     <Home/>
     {/* <Uploader/> */}
     </>
---
+++ client/src/components/utilities/card/Card.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './card.css'
+import bimage  from '../../../images/background.jpg'
+function Card(props) {
+    const image = props.imageurl
+  return (
+    <div class="card">
+  <div class="card-image-container">
+    {/* <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 1024 1024"
+      stroke-width="0"
+      fill="currentColor"
+      stroke="currentColor"
+      class="image-icon"
+    >
+      <path
+        d="M928 160H96c-17.7 0-32 14.3-32 32v640c0 17.7 14.3 32 32 32h832c17.7 0 32-14.3 32-32V192c0-17.7-14.3-32-32-32zM338 304c35.3 0 64 28.7 64 64s-28.7 64-64 64-64-28.7-64-64 28.7-64 64-64zm513.9 437.1a8.11 8.11 0 0 1-5.2 1.9H177.2c-4.4 0-8-3.6-8-8 0-1.9.7-3.7 1.9-5.2l170.3-202c2.8-3.4 7.9-3.8 11.3-1 .3.3.7.6 1 1l99.4 118 158.1-187.5c2.8-3.4 7.9-3.8 11.3-1 .3.3.7.6 1 1l229.6 271.6c2.6 3.3 2.2 8.4-1.2 11.2z"
+      ></path>
+    </svg> */}
+    <img src={image} alt="" />
+  </div>
+  <p class="card-title">{props.name}</p>
+  <p class="card-des">
+    <div className="location">{props.location}</div>
+    <div className="distance">{props.distance}</div>
+  </p>
+</div>
+
+  )
+}
+
+export default Card
